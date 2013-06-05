77776e77905dc13140a406a74ebca8702dc91bed
Cartery <CarteryNF@GMail.com>
2013-06-05 16:37:51 +0100

    Messing with the Bukkit API

---
+++ net/cartery/AspirePvanishP/AspirePvanishP.java
@@ -0,0 +1,53 @@
+package net.cartery.AspirePvanishP;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AspirePvanishP extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		getCommand("hide").setExecutor(new hideExecutor(this));
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisable has been invoked.");
+	}
+	
+
+	public boolean onCommand(CommandSender sender,  Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("hide")){
+			if(!(sender instanceof Player)) {
+				Player s = (Player)sender;
+				Player target = sender.getServer().getPlayer(args[1]);
+				target.hidePlayer(s);
+				return true;
+			}
+			return false;
+		}
+		if(cmd.getName().equalsIgnoreCase("help AspirePvanishP")){
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("--- APVP ---");
+				sender.sendMessage("/hide - Hides you");
+				sender.sendMessage("/credits - Credits");
+
+
+			}
+			if(cmd.getName().equalsIgnoreCase("credits")){
+				if(!(sender instanceof Player)) {
+					sender.sendMessage("Credits:");
+					sender.sendMessage("Coded by: Cartery11");
+					sender.sendMessage("For the AspirePvP server!");
+
+
+				}
+			return false;
+			}
+		return false;
+		}
+		return false;
+	}
+}
---
+++ net/cartery/AspirePvanishP/hideExecutor.java
@@ -0,0 +1,39 @@
+package net.cartery.AspirePvanishP;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class hideExecutor implements CommandExecutor {
+	
+	private AspirePvanishP apvp;
+	
+	
+	public hideExecutor(AspirePvanishP apvp) {
+		this.apvp = apvp;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(sender instanceof Player) {
+			Player player = (Player) sender;
+			if(player.hasPermission("AspirePvanishP.hide")) {
+				
+				return true;
+				
+			} else {
+			
+				sender.sendMessage("Not enough permissions");
+				
+			}
+			
+		} else {
+			sender.sendMessage("You must be a player.");
+			return false;
+		}
+		return false;
+	}
+		
+}
